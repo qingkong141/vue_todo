1189a29a0a63839dd3724d04c958185e9779fb1d
lsl141 <lsl141@126.com>
2020-04-05 13:41:02 +0800

    搭建项目结构(1)

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>todoList</div>
+</template>
+<script>
+export default {
+    name:'App'
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+// 1.创建vue实例
+import Vue from 'vue'
+import App from './App.Vue'
+new Vue({
+    el:'#app',
+    components:{
+        App
+    },
+    template:'<App/>'
+})
+
+//2. 挂在app组件
